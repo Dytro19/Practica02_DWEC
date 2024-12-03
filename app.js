f62e8f0ec71b05bbd610184a22018a589adc496e
Alvaro Gomez <alvagomuri@gmail.com>
2024-12-03 12:02:46 +0100

    Update app.js
    intento de configuracion de colisiones de bordes de ventana

--- app.js
+++ app.js
@@ -3,33 +3,54 @@ let X=0;
 let Y=0;
 
 //Window border X
-windowX=window.innerWidth-25+"%";
+windowX=window.innerWidth-200;
+
+//Window resize
+window.addEventListener("resize",()=>{
+    windowX=window.innerWidth-200;
+
+})
 
 //Window border X conditional
 moveX=true;
 
 //Literal object
-const OBJbasket={
+const objBasket={
+
     //Movement function
-    movebasket(){
+    moveBasket(){
+
+        //Key event
         document.addEventListener("keydown", (event)=>{
 
             const basket=document.getElementById("basket");
 
             //Movement conditions
             switch (event.key){
+
                 //Right movement
                 case "ArrowRight": case "d":
-                    X+=0.5;
-                    basket.style.marginLeft=X+"%";
-                    colider();
-                    break;
+
+                    //Window colider X conditional
+                    if(moveX==true && X<=windowX){
+                        console.log(X);
+                        console.log(windowX);
+                        X+=0.5;
+                        basket.style.marginLeft=X+"%";
+                        colider();
+                        break;
+                    }
+
                 //Left movement
                 case "ArrowLeft": case "a":
-                    X-=0.5;
-                    basket.style.marginLeft=X+"%";
-                    colider();
-                    break;
+
+                    //Window colider X conditional
+                    if(moveX==true){
+                        X-=0.5;
+                        basket.style.marginLeft=X+"%";
+                        colider();
+                        break;
+                    }
             }
         });
     },
@@ -37,13 +58,17 @@ const OBJbasket={
 
 //Window colider X
 function colider(){
+
+    //Right border
     if (windowX <= X){
         moveX = true;
     }
-    if (0 >= X){
+
+    //Left border
+    if (windowX >= X){
         moveX = false;
     }
 }
 
-//Movement exjecution
-OBJbasket.movebasket();
+//Movement ejecution
+objBasket.moveBasket();
